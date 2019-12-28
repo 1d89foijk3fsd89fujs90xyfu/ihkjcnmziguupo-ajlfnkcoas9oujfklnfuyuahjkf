19013a6a2ffde2b50b4b870b6d58305022eff46f
1d89foijk3fsd89fujs90xyfu <59311930+1d89foijk3fsd89fujs90xyfu@users.noreply.github.com>
2019-12-28 20:48:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import requests
 import urllib.request
 import json
 import asyncio
+import pymongo
+from pymongo import MongoClient
 
 bot = commands.Bot(command_prefix='?', status=discord.Status.idle, activity=discord.Game("Ligando o bot"))
 
+admin = 457914780876013569
+COR = 0x34363c
+url = os.environ["LOL"]
 channel_id = "UCJnYvI7s9PwirJSU0okv8JA"
 key = os.environ["API_KEY"]
 data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+channel_id+"&key="+key).read()
@@ -47,6 +52,37 @@ async def (ctx,cog=None):
             embed=discord.Embed(title="Erro", color=COR)
             embed.description= f"A extenção {cog} não foi carregada devido ao erro \n\n```yml\n{e}\n```"
         await ctx.send(embed=embed)
+
+@bot.command()
+@commands.guild_only()
+async def meta(ctx,cog=None):
+    if ctx.member.id == 457914780876013569:
+        if cog is None:
+            return await ctx.send("Me diga uma meta")
+        if cog is not None:
+            try:
+                mongo = MongoClient(url)
+                SeiLA = mongo["SeiLA"]
+                server = SeiLA["server"]
+                server = SeiLA.server.find_one({"_id": 871})
+                if server is None:
+                    canal = {"_id": str(871),"meta": str(cog)}
+                    SeiLA.server.insert_one(canal).inserted_id
+                    embed=discord.Embed(title="Meta setada", color=COR)
+                    embed.description = f"A nova meta é {cog}"
+                    await ctx.send(embed=embed)
+                if server is not None:
+                    SeiLA.server.update_one({"_id": 871}, {"$set":{"meta":(cog))}})
+                    embed=discord.Embed(title="Meta setada", color=COR)
+                    embed.description = f"A nova meta é {cog}"
+                    await ctx.send(embed=embed) 
+            except Exception as e:
+                embed=discord.Embed(title="Erro", color=COR)
+                embed.description= f"erro \n\n```yml\n{e}\n```"
+                await ctx.send(embed=embed) 
+    if ctx.member.id not is admin:
+        pass
+               
         
 if __name__ == "__main__":
     for extension in startup_extensions:

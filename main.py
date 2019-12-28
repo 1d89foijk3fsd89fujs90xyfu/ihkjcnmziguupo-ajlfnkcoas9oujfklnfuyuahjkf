e7435f8194c6588c5c8ff21e863ed85c04ac273a
1d89foijk3fsd89fujs90xyfu <59311930+1d89foijk3fsd89fujs90xyfu@users.noreply.github.com>
2019-12-28 22:36:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -34,26 +34,7 @@ async def on_ready():
         await asyncio.sleep(30)
         await bot.change_presence(status=None, activity=discord.Game(f"Somos agora {oi} inscritos!!"))
         await asyncio.sleep(30)
-
-@bot.command()
-@commands.guild_only()
-@commands.is_owner()
-async def recarregar(ctx,cog=None):
-    if cog is None:
-        return await ctx.send("Mencione umas das extençoes ai em baixo\n\n```yml\nanunciar\n```")
-    if cog is not None:
-        try:
-            bot.load_extension(f'{cog}')
-            bot.unload_extension(f'{cog}')
-            bot.load_extension(f'{cog}')
-            embed=discord.Embed(title="Extenção recarregada", color=COR)
-            embed.description = f"A extenção {cog}.py foi recarregada"
-        except Exception as e:
-            embed=discord.Embed(title="Erro", color=COR)
-            embed.description= f"A extenção {cog} não foi carregada devido ao erro \n\n```yml\n{e}\n```"
-        await ctx.send(embed=embed)
-               
-        
+                  
 if __name__ == "__main__":
     for extension in startup_extensions:
         try:

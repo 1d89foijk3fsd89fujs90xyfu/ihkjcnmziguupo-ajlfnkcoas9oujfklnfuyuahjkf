cb2279fea6577dd85e451cc21afea7ea4e50fda5
1d89foijk3fsd89fujs90xyfu <59311930+1d89foijk3fsd89fujs90xyfu@users.noreply.github.com>
2019-12-28 19:06:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import discord
 from discord.ext import commands
 import os
+import json
 import asyncio
 
 bot = commands.Bot(command_prefix='?', status=discord.Status.idle, activity=discord.Game("Ligando o bot"))
 
-startup_extensions = ["anunciar"]
+startup_extensions = ["anunciar", "subs"]
 TOKEN = os.environ["ACCESS_TOKEN"]
 
 @bot.event
@@ -16,7 +17,10 @@ async def on_ready():
     print("Id : {}".format(str(bot.user.id)))
     print("Discord.py versao : " + str(discord.__version__))
     print("----------------")
-    await bot.change_presence(status=None, activity=discord.Game("Canal: zFayser"))
+    while True:
+        await bot.change_presence(status=None, activity=discord.Game("Canal: zFayser"))
+        await asyncio.sleep(10)
+        await bot.change_presence(status=None, activity=discord.Game(f"Somos agora {subs} | "))
 
 if __name__ == "__main__":
     for extension in startup_extensions:

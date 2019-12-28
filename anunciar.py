0daec9e1b8c2e099b68582f56a00cae1043574f9
1d89foijk3fsd89fujs90xyfu <59311930+1d89foijk3fsd89fujs90xyfu@users.noreply.github.com>
2019-12-28 20:05:53 +0000

    Update anunciar.py

--- anunciar.py
+++ anunciar.py
@@ -7,7 +7,7 @@ import asyncio
 
 channel_id = "UCJnYvI7s9PwirJSU0okv8JA"
 api_key = os.environ["API_KEY"]
-data = urllib.request.urlopen(f"https://www.googleapis.com/youtube/v3/channels?channels?part=statistics&id={channel_id}&key={api_key}).read()
+data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+channel_id+"&key="+key).read()
 oi = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
 
 class anunciar(commands.Cog):

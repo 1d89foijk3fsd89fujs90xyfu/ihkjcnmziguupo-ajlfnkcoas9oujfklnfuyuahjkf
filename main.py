8f161afce986a59d471ec58bd075db573d401491
1d89foijk3fsd89fujs90xyfu <59311930+1d89foijk3fsd89fujs90xyfu@users.noreply.github.com>
2019-12-28 20:07:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import asyncio
 bot = commands.Bot(command_prefix='?', status=discord.Status.idle, activity=discord.Game("Ligando o bot"))
 
 channel_id = "UCJnYvI7s9PwirJSU0okv8JA"
-api_key = os.environ["API_KEY"]
+key = os.environ["API_KEY"]
 data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+channel_id+"&key="+key).read()
 oi = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
 

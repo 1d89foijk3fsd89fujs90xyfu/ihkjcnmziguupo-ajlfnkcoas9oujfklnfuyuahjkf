af4a3e076791e02e1e6a3f5b0365b67e418f61eb
1d89foijk3fsd89fujs90xyfu <59311930+1d89foijk3fsd89fujs90xyfu@users.noreply.github.com>
2019-12-28 20:59:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -52,36 +52,6 @@ async def recarregar(ctx,cog=None):
             embed=discord.Embed(title="Erro", color=COR)
             embed.description= f"A extenção {cog} não foi carregada devido ao erro \n\n```yml\n{e}\n```"
         await ctx.send(embed=embed)
-
-@bot.command()
-@commands.guild_only()
-async def meta(ctx,cog=None):
-    if ctx.member.id == 457914780876013569:
-        if cog is None:
-            return await ctx.send("Me diga uma meta")
-        if cog is not None:
-            try:
-                mongo = MongoClient(url)
-                SeiLA = mongo["SeiLA"]
-                server = SeiLA["server"]
-                server = SeiLA.server.find_one({"_id": 871})
-                if server is None:
-                    canal = {"_id": str(871),"meta": str(cog)}
-                    SeiLA.server.insert_one(canal).inserted_id
-                    embed=discord.Embed(title="Meta setada", color=COR)
-                    embed.description = f"A nova meta é {cog}"
-                    await ctx.send(embed=embed)
-                if server is not None:
-                    SeiLA.server.update_one({"_id":str(871)}, {"$set":{"meta":(str(cog))}})
-                    embed=discord.Embed(title="Meta setada", color=COR)
-                    embed.description = f"A nova meta é {cog}"
-                    await ctx.send(embed=embed) 
-            except Exception as e:
-                embed=discord.Embed(title="Erro", color=COR)
-                embed.description= f"erro \n\n```yml\n{e}\n```"
-                await ctx.send(embed=embed) 
-    if ctx.member.id not is admin:
-        pass
                
         
 if __name__ == "__main__":
